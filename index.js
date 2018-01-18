30b75f0ec4846c9382eb0c879d1cdbd09a37e87c
moonyoulove <charlie851213@gmail.com>
2018-01-18 22:02:12 +0800

    ...

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  console.log(message.content);
-  if (message.content.toString() === '有人嗎?') {
+  if (message.content === '有人嗎?') {
     message.reply('你好');
   }
 });
