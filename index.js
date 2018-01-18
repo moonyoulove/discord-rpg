87b4148f32b7aa09cde7fc102c788f297c218059
moonyoulove <charlie851213@gmail.com>
2018-01-19 00:34:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,30 @@ client.on('message', message => {
 
 client.login('NDAzMjQwNDg2ODk2MTQwMjk4.DUEdww.KYLRCZzDvdxMJs8ZgygReG-r1ow');
 
-//const keepalive = require('./keepalive.js');
+const express = require('express');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
+// pings server every 15 minutes to prevent dynos from sleeping
+//setInterval(() => {
+//  http.get('http://discord-rpg.herokuapp.com');
+//}, 900000);
